d59e225f0f04e6609187eac627d5c3f777e9b192
sanchitaggarwal <sanchitagarwal108@mil.com>
2013-07-13 18:19:46 +0530

    add lib

---
+++ js/main.js
@@ -0,0 +1,7 @@
+<script>
+// Create a YUI sandbox on your page.
+YUI().use('node', 'event', function (Y) {
+    // The Node and Event modules are loaded and ready to use.
+    // Your code goes here!
+    });
+</script>
