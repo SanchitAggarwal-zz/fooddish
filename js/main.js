e987dd633a2605c1c68e47320af7d41fb90b69af
sanchitaggarwal <sanchitagarwal108@mil.com>
2013-07-13 21:59:26 +0530

    add lib

--- js/main.js
+++ js/main.js
@@ -3,47 +3,45 @@
 //global variables
 var foodItemList;
 var topItemListURL= "http://";
-var geoLocation=getLocation();
+var hotelInfoURL="http://10.10.1.193:8080/Food/service/hotel?";
+//var geoLocation=getLocation();
+
 
 function showTopItemList(){
     $.ajax({ // ajax call starts
-        type:"POST",
+        type:"GET",
         url: topItemListURL, // Rest call for the top item list
-        data: geoLocation, // Send value of the geo location of the user
+        //data: geoLocation, // Send value of the geo location of the user
         dataType: 'json', // Choosing a JSON datatype
         success: function(data) // Variable data contains the data we get from serverside
+        {
+
+        },
+        error:function(data)
         {
 
         }
     });
 }
+
+function showHotelInfo(){
+    $.ajax({ // ajax call starts
+        type:"GET",
+        url: hotelInfoURL+"name=IMAX", // Rest call for the top item list
+        //data:data , // Send value of the geo location of the user
+        dataType: 'jsonp', // Choosing a JSON datatype
+        success: function(data) // Variable data contains the data we get from serverside
+        {
+               alert(data.hotelId);
+        },
+        error:function(data)
+        {
+            alert(data.hotelId);
+        }
+    });
+}
 //Main function on load
 $(document).ready(function(){
-
-});
-
-
-
-$(function() {
-    var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
-    $.getJSON( flickerAPI, {
-        tags: "mount rainier",
-        tagmode: "any",
-        format: "json"
-    })
-        .done(function( data ) {
-            $.each( data.items, function( i, item ) {
-                $( "<img/>" ).attr( "src", item.media.m ).appendTo( "#images" );
-                if ( i === 3 ) {
-                    return false;
-                }
-            });
-        });
-})();
-
-$.each(arr, function(count, item) {
-    var newTd = $('<td></td>').html(item).attr('name','pieTD');
-    var newTr = $('<tr></tr>');
-    newTr.append(newTd);
-    $('table').append(newTr);
+    //showTopItemList();
+    showHotelInfo();
 });
