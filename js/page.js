de6b23f903baf7f343b4b74fd368dc45ba2151fc
sanchitaggarwal <sanchitagarwal108@mil.com>
2013-07-14 08:11:08 +0530

    added dynamic html

--- js/page.js
+++ js/page.js
@@ -1,5 +1,7 @@
 // The angular module for the page
 var fooddish = angular.module('fooddish', []);
+var topItemListURL= "http://10.10.1.193:8080/Food/service/topItems";
+var hotelInfoURL="http://10.10.1.193:8080/Food/service/hotel";
 
 // The main angular controller for the page
 var fooddishController = function ($scope, $http) {
@@ -7,8 +9,8 @@ var fooddishController = function ($scope, $http) {
     $scope.error = true;
     //$scope.topItemListURL= "http://10.10.1.193:8080/Food/service/topItems";
    // $scope.hotelInfoURL="http://10.10.1.193:8080/Food/service/hotel";
-    $scope.hotelInfoURL="resources/context.json";
-    $scope.topItemListURL= "resources/randomSchema.json";
+    $scope.hotelInfoURL=hotelInfoURL;
+    $scope.topItemListURL= topItemListURL;
     // Variables related to the top food item list
     $scope.foodItemList = [];
     $http({ // Load the initial data
@@ -39,3 +41,18 @@ var fooddishController = function ($scope, $http) {
             $scope.status = status;
         });
 };
+
+
+//Main function on load
+$(document).ready(function(){
+    $('#topFoodCarousel').carousel();
+    //    showTopItemList();
+    //    showHotelInfo();
+});
+
+//To get the hotelList by Item Id
+function getHotelByItem(itemId){
+    hotelInfoURL=hotelInfoURL+"?"
+    fooddishController($scope, $http);
+
+    }
